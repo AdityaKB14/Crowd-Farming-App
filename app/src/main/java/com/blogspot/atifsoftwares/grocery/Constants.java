ceef850a5b2b62c6bbe8e6fd4c9a1641d876e5bd
ADITYA\adibh <adibhadragond007@gmail.com>
2022-07-05 00:22:29 +0530

    master

---
+++ app/src/main/java/com/blogspot/atifsoftwares/grocery/Constants.java
@@ -0,0 +1,40 @@
+package com.blogspot.atifsoftwares.grocery;
+
+public class Constants {
+
+    public static final String FCM_KEY = "AAAAa1PBqtM:APA91bENflAoZvYBnp6Jg1phFe5YpBzlNl-1Jii6VpxDjiulL8cTI6PzHZ8PyaRaPgQBgZgP3YtZ1QFRiHNT_WrxTJwazYuNGSb_8u7BDxkeaX8rB6fD9uMpJFyF_oT4hxmsnLdMCkR_";
+    public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
+
+    //product categories
+    public static final String[] productCategories = {
+            "Beverages",
+            "Beauty & Personal Care",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+    public static final String[] productCategories1 = {
+            "All",
+            "Beverages",
+            "Beauty & Personal Care",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+}
